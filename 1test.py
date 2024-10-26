a5b274525c1669d9e3ab307b44cc2f1f901f2a7a
Atharv M <112397185+AtharvM02222@users.noreply.github.com>
2024-10-26 15:45:28 +0530

    Update 1test.py

---
+++ 1test.py
@@ -0,0 +1,28 @@
+def main():
+    print("Welcome! Let's find and separate the numbers between two numbers into odd and even.")
+    
+
+    number1 = int(input("Enter the first number: "))
+    number2 = int(input("Enter the second number: "))
+    
+    # Ensure number1 is less than number2
+    if number1 > number2:
+        number1, number2 = number2, number1 
+
+
+    even_numbers = []
+    odd_numbers = []
+    
+
+    for num in range(number1, number2 + 1):
+        if num % 2 == 0:
+            even_numbers.append(num)
+        else:
+            odd_numbers.append(num)
+
+
+    print(f"The even numbers between {number1} and {number2} are: {even_numbers}")
+    print(f"The odd numbers between {number1} and {number2} are: {odd_numbers}")
+
+# Run
+main()
